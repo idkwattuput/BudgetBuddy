fd12d55638696c0d3789114e532e6d78f226aad9
idkwattuput <ikhwank322@gmail.com>
2025-03-09 02:44:24 +0800

    feat(frontend): navbar and sidebar

---
+++ frontend/src/app/(protected)/layout.tsx
@@ -0,0 +1,21 @@
+import Navbar from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
+
+export default function AuthenticateLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <div className="h-screen">
+      <Navbar />
+      <div className="flex h-full">
+        <Sidebar />
+        <div className="flex-1">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
